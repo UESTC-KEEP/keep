cfbcbe326b628b1400c1362d34c49d9debd110cc
EthanAlban <875175688@qq.com>
2021-11-12 16:59:33 +0800

    内存用量 mqtt bug

--- edge/pkg/healthzagent/mqtt/mqtt.go
+++ edge/pkg/healthzagent/mqtt/mqtt.go
@@ -12,6 +12,7 @@ import (
 
 // Message  用于解析未知的mqtt数据
 var Message map[string]interface{}
+var MqttClient *client.Client
 
 // SubscribeMqtt  用户传入需要监听的topic持续获取数据
 func SubscribeMqtt(host_ip, port, topic string) map[string]interface{} {
@@ -19,51 +20,45 @@ func SubscribeMqtt(host_ip, port, topic string) map[string]interface{} {
 	done := make(chan struct{})
 	defer close(done)
 	// build actual signal list to control
-	term := false
 
-	cli := connectToMqtt(host_ip, port)
-	for {
-		err := cli.Subscribe(&client.SubscribeOptions{
-			SubReqs: []*client.SubReq{
-				&client.SubReq{
-					TopicFilter: []byte("clock_sensor"),
-					QoS:         mqtt.QoS0,
-					// Define the processing of the message handler.
-					Handler: func(topicName, message []byte) {
-						fmt.Println(string(topicName), string(message))
-						err := json.Unmarshal(message, &Message)
-						if err != nil {
-							logger.Error(err)
-						}
-						fmt.Println("解析的结构体：", Message)
-						cancel()
-					},
+	connectToMqtt(host_ip, port)
+	err := MqttClient.Subscribe(&client.SubscribeOptions{
+		SubReqs: []*client.SubReq{
+			&client.SubReq{
+				TopicFilter: []byte(topic),
+				QoS:         mqtt.QoS1,
+				// Define the processing of the message handler.
+				Handler: func(topicName, message []byte) {
+					fmt.Println(string(topicName), string(message))
+					err := json.Unmarshal(message, &Message)
+					if err != nil {
+						logger.Error(err)
+					}
+					fmt.Println("解析的结构体：", Message)
+					cancel()
 				},
 			},
-		})
-
-		if err != nil && ctx.Err() == nil {
-			logger.Error(err)
-			continue
-		}
-
-		select {
-		// Check for termination request.
-		case <-ctx.Done():
-			logger.Debug(fmt.Sprintf("Termination pending: %s", ctx.Err()))
-			term = true
-			// sleep 1.5-2 sec before next round
-			// (recommended by specification as "collecting period")
-		case <-time.After(2000 * time.Millisecond):
-		}
-		if term {
-			break
-		}
+		},
+	})
+	if err != nil && ctx.Err() == nil {
+		logger.Error(err)
 	}
-	err := cli.Disconnect()
+
+	select {
+	// Check for termination request.
+	case <-ctx.Done():
+		logger.Debug(fmt.Sprintf("Termination pending: %s", ctx.Err()))
+		// sleep 1.5-2 sec before next round
+		// (recommended by specification as "collecting period")
+	case <-time.After(10000 * time.Millisecond):
+	}
+
+	//err := cli.Disconnect()
 	if err != nil {
 		logger.Error(err)
 	}
+
+	//defer MqttClient.Terminate()
 	return Message
 }
 
@@ -72,22 +67,24 @@ func GetRencentMqttMsg(host_ip, port, topic string) map[string]interface{} {
 	return SubscribeMqtt(host_ip, port, topic)
 }
 
-func connectToMqtt(host_ip, port string) *client.Client {
-	cli := client.New(&client.Options{
-		ErrorHandler: func(err error) {
-			logger.Error("连接mqttbroker失败...", err)
-		},
-	})
-	defer cli.Terminate()
-	// Connect to the MQTT Server.
-	err := cli.Connect(&client.ConnectOptions{
-		Network:  "tcp",
-		Address:  host_ip + ":" + port,
-		ClientID: []byte("receive-client"),
-	})
-	if err != nil {
-		logger.Fatal(err)
-		panic(err)
+func connectToMqtt(host_ip, port string) {
+	if MqttClient == nil {
+		MqttClient = client.New(&client.Options{
+			ErrorHandler: func(err error) {
+				logger.Error("连接mqttbroker失败...", err)
+			},
+		})
+		defer MqttClient.Terminate()
+		// Connect to the MQTT Server.
+		err := MqttClient.Connect(&client.ConnectOptions{
+			Network:  "tcp",
+			Address:  host_ip + ":" + port,
+			ClientID: []byte("receive-client"),
+		})
+		if err != nil {
+			logger.Fatal(err)
+			panic(err)
+		}
 	}
-	return cli
+
 }
