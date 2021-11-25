82c539eb0cc4c9a8c049ac4a86ac6934ab0a3ada
Toskies <935936455@qq.com>
2021-11-25 10:21:49 +0800

    consumer examples

--- cloud/pkg/common/kafka/examples.go
+++ cloud/pkg/common/kafka/examples.go
@@ -6,11 +6,15 @@
 package kafka
 
 import (
+	"fmt"
+	"github.com/Shopify/sarama"
 	"kafka/kafka"
+	"os"
 	"time"
 )
 var address = []string{"192.168.1.103:9092", "192.168.1.103:9093"}
 func main() {
+// producer examples
 	msgValue := make(chan string , 10)
 	topic := "topic001"
 
@@ -22,6 +26,17 @@ func main() {
 		msgValue <- value
 	}
 
+	select {}
+
+// consumer examples
+	topic := "topic001"
+	ans := make(chan *sarama.ConsumerMessage , 100)
+	groupid := "cg2"
+	go kafka.Subscribe(address,topic,groupid,ans)
+
+	for msg := range ans{
+		fmt.Fprintf(os.Stdout, "groupId=%s, topic=%s, partion=%d, offset=%d, key=%s, value=%s\n", groupid, msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
+	}
 	select {}
 }
 
