d0047dddf1d5f383217024ca6a0aa5430a38ff78
Toskies <935936455@qq.com>
2021-11-30 07:34:13 +0800

    重构struct

--- cloud/pkg/common/kafka/config.go
+++ cloud/pkg/common/kafka/config.go
@@ -4,6 +4,14 @@ import(
 	saramaCluster "github.com/bsm/sarama-cluster"
 )
 
+//var Address = []string{"192.168.1.103:9092", "192.168.1.103:9093"}
+
+type ProducerMessage = sarama.ProducerMessage // bie ming
+type ProducerError = sarama.ProducerError
+type ConsumerMessage = sarama.ConsumerMessage
+type NotifyMessage = saramaCluster.Notification
+type ConsumerError = sarama.ConsumerError
+
 type Config struct {
 	saramaCluster.Config
 	SyncProducerAmount    int
