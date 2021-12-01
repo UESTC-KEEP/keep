ba55b17e2ee153f66eb30c0a807d9e91de0e43d2
Toskies <935936455@qq.com>
2021-12-01 13:56:53 +0800

    publish & subscribe

---
+++ cloud/pkg/common/kafka/consumerconfig.go
@@ -0,0 +1,61 @@
+package kafka
+
+import (
+	"keep-edge/constants"
+	"log"
+)
+
+type ConsumerConfig struct {
+	Address []string
+	Topic string
+	GroupId string
+	Ans chan *ConsumerMessage
+}
+
+func NewConsumerConfig(topic string, groupid string) *ConsumerConfig {
+	msg := make(chan *ConsumerMessage, 100)
+	c := &ConsumerConfig{
+		Address: constants.Address,
+		//Address: []string{"192.168.1.103:9092", "192.168.1.103:9093"},
+		Topic: topic,
+		GroupId: groupid,
+		Ans: msg,
+	}
+	return c
+}
+
+
+// Subscribe 订阅/接收消息 consumer
+func (k *ConsumerConfig) Subscribe() error {
+	config :=NewConfig()
+	// ans := make(chan *sarama.ConsumerMessage)
+	c , err := InitOneConsumerOfGroup(k.Address, k.Topic, k.GroupId, config)
+	if err!=nil{
+		log.Println(err)
+		return err
+	}
+	defer c.Close()
+
+	go func() {
+		for err := range c.Errors() {
+			log.Printf("groupId=%s, Error= %s\n", c.GroupId, err.Error())
+		}
+	}()
+
+	// consume notifications
+	go func() {
+		for ntf := range c.Notifications() {
+			log.Printf("groupId=%s, Rebalanced Info= %+v \n", c.GroupId, ntf)
+		}
+	}()
+
+	for {
+		select {
+		case msg, ok := <-c.Recv():
+			if ok {
+				k.Ans <- msg
+				//fmt.Fprintf(os.Stdout, "groupId=%s, topic=%s, partion=%d, offset=%d, key=%s, value=%s\n", c.GroupId, msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
+			}
+		}
+	}
+}
