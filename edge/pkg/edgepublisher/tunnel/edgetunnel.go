df5ae18dd733c8a6877fb0a42d817a4f6871562e
heziiop <1624120705@qq.com>
2021-12-14 15:12:48 +0800

    添加了云边session未建立时的处理

--- edge/pkg/edgepublisher/tunnel/edgetunnel.go
+++ edge/pkg/edgepublisher/tunnel/edgetunnel.go
@@ -2,6 +2,7 @@ package edgetunnel
 
 import (
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"keep/constants/cloud"
@@ -122,7 +123,13 @@ func StartEdgeTunnel(nodeName, nodeIP string) {
 }
 
 func WriteToCloud(msg *model.Message) {
-	err := session.Tunnel.WriteMessage([]*model.Message{msg})
+	var err error
+	if session != nil {
+		err = session.Tunnel.WriteMessage([]*model.Message{msg})
+	} else {
+		err = errors.New("websocket session is nil")
+	}
+
 	if err != nil {
 		reconnectChan <- struct{}{}
 		_, err := beehiveContext.SendSync(modules.EdgeTwinGroup, *msg, time.Second)
@@ -133,10 +140,16 @@ func WriteToCloud(msg *model.Message) {
 }
 
 func WriteToBufferToCloud(msg *model.Message) {
+	var err error
 	msgSendBufferLock.Lock()
 	msgSendBuffer = append(msgSendBuffer, msg)
 	if len(msgSendBuffer) == edge.DefaultMsgSendBufferSize {
-		err := session.Tunnel.WriteMessage(msgSendBuffer)
+		if session != nil {
+			err = session.Tunnel.WriteMessage(msgSendBuffer)
+		} else {
+			err = errors.New("websocket session is nil")
+		}
+
 		if err != nil {
 			for _, contentMsg := range msgSendBuffer {
 				_, err := beehiveContext.SendSync(modules.EdgeTwinGroup, *contentMsg, time.Second)
